25b9968c00f209ebdd5d5dca0d620a87fad9468d
essamcomputers <essamcomputers@googlemail.com>
2026-03-03 16:46:44 +0000

    Create uninstallofficeminimal.ps1

---
+++ uninstallofficeminimal.ps1
@@ -0,0 +1,3 @@
+Start-Process "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeClickToRun.exe" `
+ -ArgumentList "scenario=install scenariosubtype=ARP sourcetype=None productstoremove=Standard2019Volume.16_en-us_x-none culture=en-us version.16=16.0 DisplayLevel=False ForceAppShutdown=True UpdatePromptUser=False" `
+ -Wait
